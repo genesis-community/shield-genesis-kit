529c6e578c83dab2ade38042c72045b1df313a78
ramonskie <ramonmakkelie@gmail.com>
2021-02-16 16:22:39 +0100

    added inital spec tests

---
+++ spec/spec_test.go
@@ -0,0 +1,35 @@
+package spec_test
+
+import (
+	"path/filepath"
+	"runtime"
+
+	. "github.com/genesis-community/testkit/testing"
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Shield Kit", func() {
+	BeforeSuite(func() {
+		_, filename, _, _ := runtime.Caller(0)
+		KitDir, _ = filepath.Abs(filepath.Join(filepath.Dir(filename), "../"))
+	})
+
+	Describe("shield", func() {
+		Test(Environment{
+			Name:        "base",
+			CloudConfig: "aws",
+			CPI:         "aws",
+		})
+		Test(Environment{
+			Name:        "oauth",
+			CloudConfig: "aws",
+			CPI:         "aws",
+		})
+		Test(Environment{
+			Name:        "postgres",
+			CloudConfig: "aws",
+			CPI:         "aws",
+			Focus:       true,
+		})
+	})
+})
