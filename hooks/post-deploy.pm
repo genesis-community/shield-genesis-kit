ac2450a7812bbfa69e52edad0a7c885cc71524c8
Kevin Rutten <krutten@fivetwenty.io>
2025-05-28 18:07:33 +0000

    Fix post-deploy hook

--- hooks/post-deploy.pm
+++ hooks/post-deploy.pm
@@ -21,16 +21,15 @@ sub perform {
   # Base class has deploy_successful method to check if GENESIS_DEPLOY_RC == 0
   if ($self->deploy_successful) {
     info(
-      "\n#M{%1} SHIELD Core deployed!\n".
+      "\n#M{$ENV{GENESIS_ENVIRONMENT}} SHIELD Core deployed!\n".
       "\nFor details about the deployment, run\n".
-      "\t#G{genesis info %1}\n".
+      "\t#G{genesis info $ENV{GENESIS_ENVIRONMENT}}\n".
       "\nTo access the SHIELD Web UI, run\n".
-      "\t#G{genesis do %1 -- open}\n".
-      "\nYou may want to configure your %1\n".
+      "\t#G{genesis do $ENV{GENESIS_ENVIRONMENT} -- open}\n".
+      "\nYou may want to configure your $ENV{GENESIS_ENVIRONMENT}\n".
       "BOSH director with an add-on, via runtime configs\n".
       "\nTo generate a good starting point, run\n".
-      "\t#G{genesis do %1 -- runtime-config}\n",
-      $ENV{GENESIS_ENVIRONMENT}
+      "\t#G{genesis do $ENV{GENESIS_ENVIRONMENT} -- runtime-config}\n"
     );
   }
 
