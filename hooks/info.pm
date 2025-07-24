3f31ba852da011f36a5330a939d9fee2b1ce0c8e
Ioannis Tsouvalas <tsouvalasi@atic.gr>
2025-07-24 08:17:11 +0000

    Refactor info hook to use generic Genesis::Hook and exodus data
    - Import `bail` and `run` from Genesis
    - Raise minimum Genesis version requirement to 3.1.0
    - Lookup Shield URL, admin username and password via `exodus_lookup`
      and bail if any are missing
    - Execute `bosh env --tty` and include its output in the info block
    - Reformat the info output to:
        • Display “Shield Information” header
        • Show BOSH environment details
        • Present Shield Web UI URL and admin credentials
        • Offer addons (open UI, print runtime-config)
    - Return `done(1)` on successful completion

--- hooks/info.pm
+++ hooks/info.pm
@@ -7,43 +7,57 @@ use warnings; # Genesis supports min perl v5.20.
 BEGIN {push @INC, $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'/.genesis/lib'}
 
 # Parent class inheritance
-use parent qw(Genesis::Hook::Info);
+use parent qw(Genesis::Hook);
 
 # Import required functions
-use Genesis qw/info/;
+use Genesis qw/info bail run/;
 
+# init - Initialize the hook {{{
 sub init {
   my ($class, %ops) = @_;
   my $obj = $class->SUPER::init(%ops);
-  $obj->check_minimum_genesis_version('3.1.0-rc.20');
+  $obj->check_minimum_genesis_version('3.1.0');
   return $obj;
 }
+# }}}
 
+# perform - Main hook execution {{{
 sub perform {
   my ($self) = @_;
 
-  my $core_name;
-  if ($self->env->has_feature('ocfp')) {
-    $core_name = $self->env->lookup('meta.core.name');
-  } else {
-    $core_name = $self->env->lookup('params.installation', 'S.H.I.E.L.D.');
-  }
+  # Get Shield URL and credentials from exodus data
+  my $shield_url = $self->env->exodus_lookup('url');
+  bail("Shield URL not found in exodus data") unless $shield_url;
+
+  my $username = $self->env->exodus_lookup('admin_username');
+  bail("Shield username not found in exodus data") unless $username;
+
+  my $password = $self->env->exodus_lookup('admin_password');
+  bail("Shield password not found in exodus data") unless $password;
+
+	my ($out,$rc,$err) = $self->env->bosh->execute(qw/env --tty/);
 
   info(
-    "\n#B{%s}\n".
-    "endpoint information\n".
-    "\t#C{%s}\n".
-    "admin credentials\n".
+    "\n#B{Shield Information}\n\n".
+    "BOSH environment:\n%s\n\n".
+    "Shield Web UI:\n".
+    "\t#C{%s}\n\n".
+    "Credentials:\n".
     "\tusername: #M{%s}\n".
-    "\tpassword: #G{%s}\n",
-    $core_name,
-    $self->exodus_data("url"),
-    $self->exodus_data("admin_username"),
-    $self->exodus_data("admin_password")
+    "\tpassword: #G{%s}\n\n".
+    "You can use the following addons:\n".
+    "\t#G{%s do open}  # Open Shield Web UI\n".
+    "\t#G{%s do runtime-config} # Print out a BOSH runtime-config\n",
+    join("\n  ", '', split("\n", $out)),
+    $shield_url,
+    $username,
+    $password,
+    scalar($self->env->get_call_path_with_env()),
+    scalar($self->env->get_call_path_with_env())
   );
 
-  return $self->done();
+  return $self->done(1);
 }
-
+# }}}
 1;
 # vim: set ts=2 sw=2 sts=2 noet fdm=marker foldlevel=1:
