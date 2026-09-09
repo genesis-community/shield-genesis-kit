940da0f45cb8491655a34797ddede7260fef552e
Wayne E. Seguin <wayneeseguin@gmail.com>
2026-09-09 12:12:57 -0400

    Resolve PVE bridge and disk storage from the OCFP vault config
    The cloud-config hooks read pve_network_bridge and pve_disk_storage only
    from bosh-configs.cpi in the environment file and fell back to the
    author-lab literals lvnet001 and zfs-1. An environment that relies on
    the vault CPI record written by ocfp vault populate rendered a cloud
    config pointing at a bridge that does not exist on the cluster.
    
    Resolve each setting from the environment file, then the OCFP vault
    config under cpi/pve, then the Proxmox defaults vmbr0 and local-lvm,
    and fail with guidance when none of them supplies a value.

--- hooks/cloud-config.pm
+++ hooks/cloud-config.pm
@@ -10,7 +10,7 @@ use parent qw(Genesis::Hook::CloudConfig);
 
 use Genesis::Hook::CloudConfig::Helpers qw/gigabytes megabytes/;
 
-use Genesis qw//;
+use Genesis qw/bail/;
 use JSON::PP;
 
 sub init {
@@ -45,7 +45,7 @@ sub perform {
 							'subnet' => $self->subnet_reference('id'),
 						},
 						pve => {
-							'bridge' => scalar($self->env->lookup('bosh-configs.cpi.pve_network_bridge', 'lvnet001')),
+							'bridge' => $self->_pve_cpi_setting('pve_network_bridge', 'network_bridge', 'vmbr0'),
 						},
 					},
 				},
@@ -95,7 +95,7 @@ sub perform {
 						'cpu'            => scalar($self->env->lookup('bosh-configs.cpi.pve_shield_cpu',  $self->for_scale({ dev => 2, prod => 4 }, 2))),
 						'ram'            => scalar($self->env->lookup('bosh-configs.cpi.pve_shield_ram',  $self->for_scale({ dev => 4096, prod => 8192 }, 4096))),
 						'disk'           => scalar($self->env->lookup('bosh-configs.cpi.pve_shield_disk', $self->for_scale({ dev => 32768, prod => 65536 }, 32768))),
-						'network_bridge' => scalar($self->env->lookup('bosh-configs.cpi.pve_network_bridge', 'lvnet001')),
+						'network_bridge' => $self->_pve_cpi_setting('pve_network_bridge', 'network_bridge', 'vmbr0'),
 					},
 				},
 			),
@@ -120,7 +120,7 @@ sub perform {
 						'type' => 'gp3',
 					},
 					pve => {
-						'storage'     => scalar($self->env->lookup('bosh-configs.cpi.pve_disk_storage', 'zfs-1')),
+						'storage'     => $self->_pve_cpi_setting('pve_disk_storage', 'disk_storage', 'local-lvm'),
 						'disk_format' => scalar($self->env->lookup('bosh-configs.cpi.pve_disk_format', 'raw')),
 					},
 				},
@@ -150,6 +150,24 @@ sub get_sgs_by_names {
 	# TODO: Error checking
 	return \@ids
 }
+
+
+# _pve_cpi_setting - resolve a PVE CPI setting from the env file, then the OCFP vault config, then a default {{{
+sub _pve_cpi_setting {
+	my ($self, $env_key, $vault_key, $default) = @_;
+	my $value = scalar($self->env->lookup("bosh-configs.cpi.$env_key", undef));
+	$value //= scalar($self->env->ocfp_config_lookup("cpi.pve.$vault_key", undef));
+	$value //= $default;
+	bail(
+		"No PVE %s configured for %s: set #c{bosh-configs.cpi.%s} in the ".
+		"environment file, or run #g{ocfp vault populate} so the OCFP config ".
+		"provides #c{cpi/pve:%s}.",
+		$vault_key, $self->env->name, $env_key, $vault_key
+	) unless defined($value) && length($value);
+	return $value;
+}
+
+# }}}
 
 1;
 # vim: set ts=2 sw=2 sts=2 noet fdm=marker foldlevel=1:
